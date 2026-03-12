f20e9d6768494f5e80311c6dc0870ce1594e2251
doingmov <yohei13111@mail.ru>
2026-03-12 19:16:28 +0300

    fill with data field Flat.construction_year

---
+++ property/migrations/0005_auto_20260312_1913.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+
+def fill_new_building(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    for flat in Flat.objects.all():
+        if flat.construction_year and flat.construction_year >= 2015:
+            flat.new_building = True
+        elif flat.construction_year and flat.construction_year < 2015:
+            flat.new_building = False
+        else:
+            flat.new_building = None
+        flat.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('property', '0004_flat_new_building'),
+    ]
+
+    operations = [
+        migrations.RunPython(fill_new_building),
+    ]
